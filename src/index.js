3c2bd07b9765ba6c59db4b8d03aa587e193d7dae
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2023-12-19 16:25:00 +0530

    change 01

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import Layout from './pages/Layout';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
