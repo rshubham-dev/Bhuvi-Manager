ac1ea79bfcd652eb26e715bc7f19b8e64de9c672
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2023-12-25 22:09:28 +0530

    update 01

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,10 +26,12 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${property.BASE_URL}/api/v1/user/login`, formData);
+      const response = await axios.post(`${property.BASE_URL}/api/v1/user/login`, formData, {
+        credentials: "include"
+      });
       console.log('Login submitted with data:', response.data);
-      Cookies.set('accessToken', response.data.accessToken);
-      Cookies.set('refreshToken', response.data.refreshToken);
+      // Cookies.set('accessToken', response.data.accessToken);
+      // Cookies.set('refreshToken', response.data.refreshToken);
       toast.success(response.data.message);
       setRole(response.data.user.role);
     } catch (error) {
