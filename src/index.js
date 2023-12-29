32ffb191dda6c76a52d5f9d372149ca67dd04aeb
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2023-12-29 23:18:30 +0530

    ui update 6.1

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import Layout from './pages/Layout';
 import axios from 'axios';
+import { UserContextProvider } from './context/AuthContext';
 
 axios.defaults.baseURL = 'https://bhuvi-management-server.onrender.com';
 axios.defaults.withCredentials = true;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Layout/>
-    <App />
-    </BrowserRouter>
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Layout />
+          <App />
+        </BrowserRouter>
+      </UserContextProvider>
+    </React.Suspense>
   </React.StrictMode>
 );
-
-
