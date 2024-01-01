941b45ffc4350673831b10fbf375b135a39c9479
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-01 20:06:04 +0530

    test cookie 1.2

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Cookies } from 'react-cookie';
 
-const cookies = Cookies();
+const cookies = new Cookies();
 const token = cookies.get('accessToken');
 console.log(token); // Check if the token is correctly retrieved
 
