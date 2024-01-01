19d363f8b194784c639e8eeb2202ffe1c60ef1e9
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-01 23:23:01 +0530

    test 2.6

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,13 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Cookies } from 'react-cookie';
-
-const cookies = new Cookies();
-const tokens = cookies.get('accessToken');
+// import { Cookies } from 'react-cookie';
+// const cookies = new Cookies();
+// const tokens = cookies.get('accessToken');
 const token = sessionStorage.getItem("token");
 
-console.log(tokens, token); // Check if the token is correctly retrieved
+console.log(token); // Check if the token is correctly retrieved
 
-const initialState = tokens || token
+const initialState = token
 ? { isLoggedIn: true, user: {}}
 : { isLoggedIn: false, user: null };
 
