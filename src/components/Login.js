e9305beada74f356c25a35690e1021e5beb55d72
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-01 16:51:55 +0530

    update 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -33,7 +33,7 @@ const LoginForm = () => {
       const response = await axios.post('/api/v1/user/login', formData);
       console.log('Login submitted with data:', response.data);
       toast.success(response.data.message);
-      if (response.data.user) {
+      if (!response.data.user) {
         dispatch(logout())
       }
       else{
