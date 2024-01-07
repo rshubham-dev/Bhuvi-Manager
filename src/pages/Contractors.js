369a1fef9565ca77da3db4ecac4e1a90e3a52e6a
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-07 18:02:56 +0530

    contractor update 1

--- src/pages/Contractors.js
+++ src/pages/Contractors.js
@@ -47,8 +47,9 @@ const Contractors = () => {
     <table className="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" className="px-6 py-3">contractor Name</th>
-                <th scope="col" className="px-6 py-3">Phone</th>
+                <th scope="col" className="px-6 py-3">Contractor</th>
+                <th scope="col" className="px-6 py-3">Contact No</th>
+                <th scope="col" className="px-6 py-3">Total Sites</th>
                 <th scope="col" className="px-6 py-3">Actions</th>
             </tr>
         </thead>
@@ -56,7 +57,8 @@ const Contractors = () => {
             {contractors.map((contractor) => (
                 <tr key={contractor.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td className="px-6 py-4">{contractor.name}</td>
-                    <td className="px-6 py-4">{contractor.phone}</td>
+                    <td className="px-6 py-4">{contractor.contactNo},{contractor.whatsapp}</td>
+                    <td className="px-6 py-4">{contractor.site.length}</td>
                     <td className="px-6 py-4">
                         <button
                             onClick={() => handleEdit(contractor._id)}
