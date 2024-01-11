43ea60f85800dc27ef41e80b5e35f9e2af3bd59c
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-11 15:41:16 +0530

    ui update 2

--- src/pages/PaymentSchedule.js
+++ src/pages/PaymentSchedule.js
@@ -33,9 +33,6 @@ const PaymentSchedules = () => {
     navigate(`/edit-payment-schedule?payment-ScheduleId=${paymentScheduleId}`);
   };
 
-  const handleRedirect = (paymentScheduleId) => {
-    navigate(`/payment-schedule?payment-scheduleId=${paymentScheduleId}`);
-  }
   const handleDelete = async (id) => {
     try {
       await axios.delete(`/api/v1/payment-schedule/${id}`);
@@ -51,14 +48,14 @@ const PaymentSchedules = () => {
 
   return (
     <div className="overflow-x-auto shadow-md sm:rounded-lg">
-    <h1 className="text-2xl font-bold text-center mt-4">Payment Schedules</h1>
-    <div className=" mb-4 mr-20 text-right">
-      <button onClick={handleAdd} className="bg-green-500 text-white px-4 py-2">
-        Add Payment Schedule
-      </button>
-    </div>
+      <h1 className="text-2xl font-bold text-center mt-4">Payment Schedules</h1>
+      <div className=" mb-4 mr-20 text-right">
+        <button onClick={handleAdd} className="bg-green-500 text-white px-4 py-2">
+          Add Payment Schedule
+        </button>
+      </div>
 
-    <section className='bg-white px-12 py-8 mb-16 h-full w-2/5'>
+      {/* <section className='bg-white px-12 py-8 mb-16 h-full w-full'>
         <div className="mt-6 w-full">
           {paymentSchedules.map((paymentSchedule) => (
             <div key={paymentSchedule._id} className="card">
@@ -69,23 +66,36 @@ const PaymentSchedules = () => {
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3">Work</th>
-                                    <th scope="col" className="px-6 py-3">Rate</th>
-                                    <th scope="col" className="px-6 py-3">Area</th>
+                                    <th scope="col" className="px-8 py-3">Work</th>
                                     <th scope="col" className="px-6 py-3">Amount</th>
+                                    <th scope="col" className="px-6 py-3">Payment Date</th>
                                     <th scope="col" className="px-6 py-3">Status</th>
+                                    <th scope="col" className="px-6 py-3">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {paymentSchedule?.paymentDetails.map((work) => (
                                     <tr key={work._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <td className="px-6 py-4">
+                                        <td className="px-8 py-4">
                                         {work.workDescription}
                                         </td>
-                                        <td className="px-6 py-4">{work.rate}</td>
-                                        <td className="px-6 py-4">{work.area}</td>
                                         <td className="px-6 py-4">{work.amount}</td>
-                                        <td className="px-6 py-4">{work.status}</td>
+                                        <td className="px-6 py-4">{work.dateOfPayment}</td>
+                                        <td className="px-6 py-4">{work.status}</td>  
+                                        <td className="px-6 py-4">
+                                                        <button
+                  onClick={() => handleEdit(work._id)}
+                  className="bg-blue-500 text-white px-2 py-1 mr-2"
+                >
+                  <GrEdit />
+                </button>
+                <button
+                  onClick={() => handleDelete(work._id)}
+                  className="bg-red-500 text-white px-2 py-1 mr-2"
+                >
+                  <MdDelete />
+                </button>
+                </td>  
                                     </tr>
                                 ))}
                             </tbody>
@@ -94,13 +104,46 @@ const PaymentSchedules = () => {
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
 
-    <Toaster
-      position="top-right"
-      reverseOrder={false}
-    />
-  </div>
+      <section className='bg-white px-8 py-2 mb-16 h-full w-full'>
+        <div className="mt-6 w-full">
+          <div className="card">
+            <details className="rounded-lg bg-white overflow-hidden shadow-lg p-3">
+              <summary className='flex justify-between flex-row text-xl font-large text-color-title cursor-pointer' style={{ padding: '1rem' }}>
+                Payment Schedule of
+              </summary>
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">Work</th>
+                    <th scope="col" className="px-6 py-3">Amount</th>
+                    <th scope="col" className="px-6 py-3">Payment Date</th>
+                    <th scope="col" className="px-6 py-3">Status</th>
+                    <th scope="col" className="px-6 py-3">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="px-6 py-4">
+
+                    </td>
+                    <td className="px-6 py-4"></td>
+                    <td className="px-6 py-4"></td>
+                    <td className="px-6 py-4"></td>
+                    <td className="px-6 py-4"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </details>
+          </div>
+        </div>
+      </section>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+    </div>
   )
 }
 
