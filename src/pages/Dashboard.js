48d9e487cfe031235eb355a060b48845ffd889d2
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-28 18:20:20 +0530

    old update

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch, } from 'react-redux';
 import { useNavigate} from 'react-router-dom';
+import Admin from './Admin.js';
+import Client from './Client.js';
+import Accountant from './Accountant.js';
+import Marketing from './Marketing.js';
+import SiteIncharge from './SiteIncharge.js';
+import SiteSupervisour from './SiteSupervisour.js';
+import Design_Head from './Design_Head.js';
+import Design_Engineer from './Design_Engineer.js';
+import Quality_Engineer from './Quality_Engineer.js';
+import Home from './Home.js';
 import { logout } from '../features/auth/authSlice.js';
 
  const Dashboard = () => {
@@ -10,69 +20,85 @@ import { logout } from '../features/auth/authSlice.js';
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
-  useEffect(() => {
-    const isPageReloaded = performance.navigation.type === 1;
-    if (isPageReloaded) {
-      // dispatch(logout());
-      // navigate('/login')
-      console.log('Reloaded')
-    } else {
-      console.log( "This page is not reloaded");
-    }
-  }, []);
-  if(window.performance){
-    console.log('test', window.performance)
-  }
+  // useEffect(() => {
+  //   const isPageReloaded = performance.navigation.type === 1;
+  //   if (isPageReloaded) {
+  //     dispatch(logout());
+  //     navigate('/login')
+  //     console.log('Reloaded')
+  //   } else {
+  //     console.log( "This page is not reloaded");
+  //   }
+  // }, []);
+  // if(window.performance){
+  //   console.log('test', window.performance)
+  // }
   // console.log('state', window.performance.navigation.type)
 
   useEffect(() => {
-    const handleNavigation = () => {
-      if (isLoggedIn) {
-        switch (user.department) {
-          case 'Admin':
-            navigate('/admin');
-            break;
-          case 'Company':
-            navigate('/admin');
-            break;
-          case 'Client':
-            navigate('/Client-dashboard');
-            break;
-          case 'Accountant':
-            navigate('/accountant');
-            break;
-          case 'Marketing':
-            navigate('/marketing');
-            break;
-          case 'Ceo':
-            navigate('/admin');
-            break;
-          case 'Site Incharge':
-            navigate('/site-incharge');
-            break;
-          case 'Site Supervisor':
-            navigate('/site-supervisour');
-            break;
-          case 'Design Head':
-            navigate('/design-head');
-            break;
-          case 'Design Engineer':
-            navigate('/design-engineer');
-            break;
-          case 'Quality Engineer':
-            navigate('/quality-engineer');
-            break;
-          default:
-            console.log("Not exists");
-            break;
-        }
-      } else {
-        navigate('/login')
+    if(isLoggedIn === true && user){
+      handleNavigation(user);
+    } else {
+      navigate('/login')
+    }
+  }, [isLoggedIn]);
+  const handleNavigation = (user) => {
+      switch (user.department) {
+        case 'Company':
+          return (
+            <Admin/> 
+            )
+          break;
+        case 'Client':
+          return (
+            <Client/> 
+            )
+          break;
+        case 'Accountant':
+          return (
+            <Accountant/> 
+            )
+          break;
+        case 'Marketing':
+          return (
+            <Marketing/> 
+            )
+          break;
+        case 'Ceo':
+          return (
+            <Admin/> 
+            )
+          break;
+        case 'Site Incharge':
+          return (
+            <SiteIncharge/> 
+            )
+          break;
+        case 'Site Supervisor':
+          return (
+            <SiteSupervisour/> 
+            )
+          break;
+        case 'Design Head':
+          return (
+            <Design_Head/> 
+            )
+          break;
+        case 'Design Engineer':
+          return (
+            <Design_Engineer/> 
+            )
+          break;
+        case 'Quality Engineer':
+          return (
+          <Quality_Engineer/> 
+          )
+          break;
+        default:
+          console.log("Not exists");
+          break;
       }
-    };
-    handleNavigation();
-  }, [isLoggedIn, navigate]);
-
+  };
 
 }
 
