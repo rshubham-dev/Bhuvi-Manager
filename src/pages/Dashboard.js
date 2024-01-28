e35e202e8fbb05e3f908b5b14321a678fc0eec2d
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-28 18:42:52 +0530

    ui update 2

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate} from 'react-router-dom';
-import Admin from './Admin.js';
-import Client from './Client.js';
-import Accountant from './Accountant.js';
-import Marketing from './Marketing.js';
-import SiteIncharge from './SiteIncharge.js';
-import SiteSupervisour from './SiteSupervisour.js';
-import Design_Head from './Design_Head.js';
-import Design_Engineer from './Design_Engineer.js';
-import Quality_Engineer from './Quality_Engineer.js';
+import { useNavigate } from 'react-router-dom';
 
- const Dashboard = () => {
+const Dashboard = () => {
   const { user, isLoggedIn } = useSelector((state) => {
     return state.auth
   });
@@ -19,70 +10,60 @@ import Quality_Engineer from './Quality_Engineer.js';
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(isLoggedIn === true && user){
-      handleNavigation(user);
-    } else {
-      navigate('/login')
-    }
-  }, [isLoggedIn]);
-  const handleNavigation = (user) => {
-      switch (user.department) {
-        case 'Company':
-          return (
-            <Admin/> 
-            )
-          break;
-        case 'Client':
-          return (
-            <Client/> 
-            )
-          break;
-        case 'Accountant':
-          return (
-            <Accountant/> 
-            )
-          break;
-        case 'Marketing':
-          return (
-            <Marketing/> 
-            )
-          break;
-        case 'Ceo':
-          return (
-            <Admin/> 
-            )
-          break;
-        case 'Site Incharge':
-          return (
-            <SiteIncharge/> 
-            )
-          break;
-        case 'Site Supervisor':
-          return (
-            <SiteSupervisour/> 
-            )
-          break;
-        case 'Design Head':
-          return (
-            <Design_Head/> 
-            )
-          break;
-        case 'Design Engineer':
-          return (
-            <Design_Engineer/> 
-            )
-          break;
-        case 'Quality Engineer':
-          return (
-          <Quality_Engineer/> 
-          )
-          break;
-        default:
-          console.log("Not exists");
-          break;
+
+    const handleNavigation = () => {
+
+      if (isLoggedIn) {
+
+        switch (user.department) {
+          case 'Admin':
+            navigate('/admin');
+            break;
+          case 'Company':
+            navigate('/admin');
+            break;
+          case 'Client':
+            navigate('/Client-dashboard');
+            break;
+          case 'Accountant':
+            navigate('/accountant');
+            break;
+          case 'Marketing':
+            navigate('/marketing');
+            break;
+          case 'Ceo':
+            navigate('/admin');
+            break;
+          case 'Site Incharge':
+            navigate('/site-incharge');
+            break;
+          case 'Site Supervisor':
+            navigate('/site-supervisour');
+            break;
+          case 'Design Head':
+            navigate('/design-head');
+            break;
+          case 'Design Engineer':
+            navigate('/design-engineer');
+            break;
+          case 'Quality Engineer':
+            navigate('/quality-engineer');
+            break;
+          default:
+            console.log("Not exists");
+            break;
+        }
+
+      } 
+      else {
+        navigate('/login')
       }
-  };
+
+    };
+    handleNavigation();
+  }, [isLoggedIn, navigate]);
 
 }
 
 export default Dashboard;
+
