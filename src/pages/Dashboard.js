58da7702fd0359ae2081461181705dc5a402ec5a
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-01-28 18:17:51 +0530

    ui error update 1

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -10,68 +10,69 @@ import { logout } from '../features/auth/authSlice.js';
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   const isPageReloaded = performance.navigation.type === 1;
-  //   if (isPageReloaded) {
-  //     dispatch(logout());
-  //     navigate('/login')
-  //     console.log('Reloaded')
-  //   } else {
-  //     console.log( "This page is not reloaded");
-  //   }
-  // }, []);
-  // if(window.performance){
-  //   console.log('test', window.performance)
-  // }
+  useEffect(() => {
+    const isPageReloaded = performance.navigation.type === 1;
+    if (isPageReloaded) {
+      // dispatch(logout());
+      // navigate('/login')
+      console.log('Reloaded')
+    } else {
+      console.log( "This page is not reloaded");
+    }
+  }, []);
+  if(window.performance){
+    console.log('test', window.performance)
+  }
   // console.log('state', window.performance.navigation.type)
 
   useEffect(() => {
-    if(isLoggedIn === true && user){
-      handleNavigation(isLoggedIn);
-    } else {
-      navigate('/login')
-    }
-  }, [isLoggedIn]);
-  const handleNavigation = (user) => {
-    switch (user.department) {
-      case 'Admin':
-        navigate('/admin');
-        break;
-      case 'Company':
-        navigate('/admin');
-        break;
-      case 'Client':
-        navigate('/Client-dashboard');
-        break;
-      case 'Accountant':
-        navigate('/accountant');
-        break;
-      case 'Marketing':
-        navigate('/marketing');
-        break;
-      case 'Ceo':
-        navigate('/admin');
-        break;
-      case 'Site Incharge':
-        navigate('/site-incharge');
-        break;
-      case 'Site Supervisor':
-        navigate('/site-supervisour');
-        break;
-      case 'Design Head':
-        navigate('/design-head');
-        break;
-      case 'Design Engineer':
-        navigate('/design-engineer');
-        break;
-      case 'Quality Engineer':
-        navigate('/quality-engineer');
-        break;
-      default:
-        console.log("Not exists");
-        break;
-    }
-  };
+    const handleNavigation = () => {
+      if (isLoggedIn) {
+        switch (user.department) {
+          case 'Admin':
+            navigate('/admin');
+            break;
+          case 'Company':
+            navigate('/admin');
+            break;
+          case 'Client':
+            navigate('/Client-dashboard');
+            break;
+          case 'Accountant':
+            navigate('/accountant');
+            break;
+          case 'Marketing':
+            navigate('/marketing');
+            break;
+          case 'Ceo':
+            navigate('/admin');
+            break;
+          case 'Site Incharge':
+            navigate('/site-incharge');
+            break;
+          case 'Site Supervisor':
+            navigate('/site-supervisour');
+            break;
+          case 'Design Head':
+            navigate('/design-head');
+            break;
+          case 'Design Engineer':
+            navigate('/design-engineer');
+            break;
+          case 'Quality Engineer':
+            navigate('/quality-engineer');
+            break;
+          default:
+            console.log("Not exists");
+            break;
+        }
+      } else {
+        navigate('/login')
+      }
+    };
+    handleNavigation();
+  }, [isLoggedIn, navigate]);
+
 
 }
 
