905b7a829fd7e2855b4632b3eaae722148850ee3
Shubham Kuamr <Shubhamkrg1819@gmail.com>
2024-02-10 19:38:50 +0530

    layout update 1.3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -17,60 +17,75 @@ const AntDLayout = ({ children }) => {
   });
 
   return (
-    <Layout
+<Layout
+  style={{
+    width: '100%',
+    minHeight: '100%', // Set minimum height to 100% of viewport height
+    margin: '0px',
+    padding: '0px',
+    background: colorBgContainer,
+  }}
+>
+  {isLoggedIn ? (
+    <Sider
+      trigger={null}
+      collapsible
+      collapsed={collapsed}
+      breakpoint="md" // Set breakpoint for responsive behavior
+      onBreakpoint={(broken) => {
+        setCollapsed(broken);
+      }}
+      style={{
+        overflow: 'auto',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        zIndex: 1, // Ensure the sidebar remains above other content
+        scrollbarWidth: 'none', // Hide scrollbar in Firefox
+        '-ms-overflow-style': 'none', // Hide scrollbar in IE and Edge
+      }}
+    >
+      <img
+        className="rounded-full mx-auto h-10 w-auto sm:h-10 md:h-12 lg:h-14 my-4 text-center object-center"
+        src={logo}
+        alt='logo'
+      />
+      <MenuList />
+    </Sider>
+  ) : null}
+  <Layout
     style={{
       width: '100%',
-      height: '100%',
+      minHeight: '100vh', // Set minimum height to 100% of viewport height
       margin: '0px',
       padding: '0px',
       background: colorBgContainer,
-    }}>
-    {isLoggedIn ?
-      <Sider
-        trigger={null}
-        collapsible 
-        collapsed={collapsed}
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: "sticky",
-          left: 0,
-          top: 0,
-          bottom: 0,
-          scrollbarWidth: 'none', // Hide scrollbar in Firefox
-          '-ms-overflow-style': 'none', // Hide scrollbar in IE and Edge
-        }}>
-        <img
-        className="rounded-full mx-auto h-10 w-auto sm:h-10 md:h-12 lg:h-14 my-4 text-center object-center"
-        src={logo} 
-        alt='logo'/>
-        <MenuList />
-      </Sider> : ''}
-    <Layout>
-      <Header
-        style={{
-          padding: 0,
-          background: colorBgContainer,
-        }}
-      >
-        <Navbar collapsed={collapsed} setCollapsed={setCollapsed} />
-      </Header>
-      <Content
-        style={{
-          margin: '24px 14px',
-          padding: '20px',
-          Height: '100%',
-          maxWidth: '100%',
-          borderRadius: borderRadiusLG,
-          background: colorBgContainer,
-          overflow: "initial"
-        }}
-        >
-        {children}
-      </Content>
-    </Layout>
+    }}
+  >
+    <Header
+      style={{
+        padding: 0,
+        background: colorBgContainer,
+        zIndex: 0, // Ensure the header remains behind the sidebar
+      }}
+    >
+      <Navbar collapsed={collapsed} setCollapsed={setCollapsed} />
+    </Header>
+    <Content
+      style={{
+        margin: '24px 14px',
+        padding: '20px',
+        borderRadius: borderRadiusLG,
+        background: colorBgContainer,
+        overflow: "initial"
+      }}
+    >
+      {children}
+    </Content>
   </Layout>
-  );
+</Layout>
+
+);
 }
 
 export default AntDLayout
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,59 +38,60 @@ const Navbar = ({ collapsed, setCollapsed }) => {
 
 
   return (
-    <nav className="bg-gray-50 text-gray min-w-screen max-w-full border-b border-gray-200 py-3 px-1 md:px-6 lg:px-10 dark:fill-gray-400 dark:bg-gray-800">
-      <div className="flex flex-row items-center justify-between container mx-auto space-x-4">
+<nav className="bg-gray-50 text-gray min-w-screen max-w-screen border-b border-gray-200 py-3 px-1 md:px-6 lg:px-10 dark:fill-gray-400 dark:bg-gray-800">
+  <div className="flex flex-row items-center justify-between container mx-auto space-x-4">
 
-        <div className="flex items-center justify-between space-x-2">
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '18px',
-              width: 40,
-              height: 40,
-              display: isLoggedIn ? 'block' : 'none',
-            }}
-          />
+    <div className="flex items-center justify-between space-x-2">
+      <Button
+        type="text"
+        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        onClick={() => setCollapsed(!collapsed)}
+        style={{
+          fontSize: '18px',
+          width: 40,
+          height: 40,
+          display: isLoggedIn ? 'block' : 'none',
+        }}
+      />
 
-          <div className="px-4 sm:hidden">
-            <h2 className=" font-bold text-gray text-sm sm:text-sm md:text-xl lg:text-2xl whitespace-nowrap">Bhuvi Consultants</h2>
-          </div>
-        </div>
-
-        <div className="flex items-center space-x-3 lg:space-x-6 md:space-x-4 sm:space-x-2" >
-          <Badge
-            count="1"
-            onClick={() => {
-              navigate('/message');
-            }}
-            size="small"
-            className="text-gray flex flex-col items-center dark:text-white"
-          >
-            <MdNotifications className="text-lg text-gray dark:text-white lg:text-2xl" />
-          </Badge>
-
-          {isLoggedIn ? (
-            <NavLink onClick={logOut} className="text-lg md:text-xl lg:text-2xl">
-              <MdLogout className="text-gray" />
-            </NavLink>
-          ) : (
-            <NavLink to="/login" className="text-gray dark:text-white text-lg md:text-xl lg:text-2xl">
-              <MdLogin />
-            </NavLink>
-          )}
-          <div className="photo-wrapper">
-            <img
-              className="lg:w-12 lg:h-12 w-8 h-8 md:w-9 md:h-9 rounded-full mx-auto"
-              src={user?.avatar || image}
-              alt={user?.userName}
-            />
-          </div>
-        </div>
+      <div className="px-4">
+        <h2 className="font-bold text-gray text-sm sm:text-sm md:text-xl lg:text-2xl whitespace-nowrap">Bhuvi Consultants</h2>
       </div>
-      <Toaster position="top-right" reverseOrder={false} />
-    </nav>
+    </div>
+
+    <div className="flex items-center space-x-3 lg:space-x-6 md:space-x-4 sm:space-x-2">
+      <Badge
+        count="1"
+        onClick={() => {
+          navigate('/message');
+        }}
+        size="small"
+        className="text-gray flex flex-col items-center dark:text-white"
+      >
+        <MdNotifications className="text-lg text-gray dark:text-white lg:text-2xl" />
+      </Badge>
+
+      {isLoggedIn ? (
+        <NavLink onClick={logOut} className="text-lg md:text-xl lg:text-2xl">
+          <MdLogout className="text-gray" />
+        </NavLink>
+      ) : (
+        <NavLink to="/login" className="text-gray dark:text-white text-lg md:text-xl lg:text-2xl">
+          <MdLogin />
+        </NavLink>
+      )}
+      <div className="photo-wrapper">
+        <img
+          className="lg:w-12 lg:h-12 w-8 h-8 md:w-9 md:h-9 rounded-full mx-auto"
+          src={user?.avatar || image}
+          alt={user?.userName}
+        />
+      </div>
+    </div>
+  </div>
+  <Toaster position="top-right" reverseOrder={false} />
+</nav>
+
 
   );
 };
